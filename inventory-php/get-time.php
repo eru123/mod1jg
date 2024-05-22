267803eb1e7f76094424e4bd64548b549b3fe1e7
eru123 <yeoligoakino@gmail.com>
2024-05-22 13:24:53 +0800

    remove unused files

--- inventory-php/get-time.php
+++
@@ -1,3 +0,0 @@
-<?php
-date_default_timezone_set('Asia/Manila');
-echo 'Time: ' .  date("g:i:sA");
--- inventory-php/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once __DIR__ . '/global.php';
-session_start();
-session_destroy();
-header("location: " . base_url());
